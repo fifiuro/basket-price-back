06786fba65f261e51f1d9e25d2c641a004a46bf8
fifiuro <fifiuro@gmail.com>
2024-09-13 12:13:34 -0400

    Otro mas

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('API Example')
-    .setDescription('The API description')
+    .setTitle('Canasta Familiar')
+    .setDescription('Registro de los precios de la Canasta Familiar...')
     .setVersion('1.0')
     .build();
 
