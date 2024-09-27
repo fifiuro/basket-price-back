11950908aee932c22cd5cefb98b28e22ffe15b93
fifiuro <fifiuro@gmail.com>
2024-09-26 20:53:11 -0400

    Christian: Conexion a base de datos y estructura de proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DepartmentModule } from './modules/department/department.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'admin',
+      username: 'postgres',
+      entities: [],
+      database: 'basket-price',
+      synchronize: true,
+      logging: true,
+    }),
+    DepartmentModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
